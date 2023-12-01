71e3db0063b47b84b2a6e684c60d70ccf5a66ff2
kgpark24 <79816486+kanggongp@users.noreply.github.com>
2023-12-01 18:54:46 +0900

    scss option 설정

--- src/components/views/Home/HomeContainer.tsx
+++ src/components/views/Home/HomeContainer.tsx
@@ -1,6 +1,7 @@
+import styles from './HomeContainer.module.scss'
 const HomeContainer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>home container2</div>
     </div>
   );
