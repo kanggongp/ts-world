4f56c721274380c6714ab010ac2d1b462abfd163
kgpark24 <79816486+kanggongp@users.noreply.github.com>
2023-12-13 13:04:11 +0900

    next image 설정

--- next.config.js
+++ next.config.js
@@ -6,7 +6,20 @@ const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePath: [path.join(__dirname, 'styles')],
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.ap-northeast-2.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'gongpark-toyproject.s3.ap-northeast-2.amazonaws.com',
+      },
+    ],
+    domains: ['https://s3.ap-northeast-2.amazonaws.com'],
+  },
 }
 
 module.exports = nextConfig
--- src/components/views/Home/HomeContainer.tsx
+++ src/components/views/Home/HomeContainer.tsx
@@ -1,8 +1,18 @@
 import styles from './HomeContainer.module.scss'
+import Image from "next/image";
 const HomeContainer = () => {
+
+  const imgSrc = 'https://gongpark-toyproject.s3.ap-northeast-2.amazonaws.com/stattoo/home/stattoo1.png'
+
   return (
     <div className={styles.container}>
-      <div>home container layout</div>
+      <div className={styles.mainBanner}>
+        <Image
+          fill
+          src={imgSrc}
+          alt={'img'}
+        />
+      </div>
     </div>
   );
 };
