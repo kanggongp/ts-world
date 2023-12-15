e39f1a4f7217e25adc0a155daee7b63d8feef7c2
kgpark24 <79816486+kanggongp@users.noreply.github.com>
2023-12-15 17:50:43 +0900

    test page setting

--- src/components/views/Test/TestContainer.tsx
+++ src/components/views/Test/TestContainer.tsx
@@ -3,9 +3,19 @@ import styles from './TestContainer.module.scss'
 
 const TestContainer = () => {
 
+  const testFunction = () => {
+
+    const result = 1 > 0 && 'hi'
+
+    console.log(result)
+  }
+
   return (
     <div className={styles.container}>
       test
+      <div>
+        <button onClick={testFunction}>click</button>
+      </div>
     </div>
   )
 }
