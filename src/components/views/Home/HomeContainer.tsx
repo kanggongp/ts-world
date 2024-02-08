eac283e6e7a8db923148f0307af906209aedce60
kanggongp <gongkang24@gmail.com>
2024-02-08 12:18:30 +0900

    이전 데이터 백업

--- src/components/views/Home/HomeContainer.tsx
+++ src/components/views/Home/HomeContainer.tsx
@@ -1,18 +1,8 @@
 import styles from './HomeContainer.module.scss'
-import Image from "next/image";
 const HomeContainer = () => {
-
-  const imgSrc = 'https://gongpark-toyproject.s3.ap-northeast-2.amazonaws.com/stattoo/home/stattoo1.png'
 
   return (
     <div className={styles.container}>
-      <div className={styles.mainBanner}>
-        <Image
-          fill
-          src={imgSrc}
-          alt={'img'}
-        />
-      </div>
       <div className={styles.midContainer}>
         deploy test23232
       </div>
--- src/components/views/Test/TestContainer.tsx
+++ src/components/views/Test/TestContainer.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import styles from './TestContainer.module.scss'
+import Image from "next/image";
 
 const TestContainer = () => {
 
-  const testFunction = () => {
-
-    const result = 1 > 0 && 'hi'
-
-    console.log(result)
-  }
+  const imgSrc = 'https://gongpark-toyproject.s3.ap-northeast-2.amazonaws.com/stattoo/home/stattoo1.png'
 
   return (
     <div className={styles.container}>
-      test
-      <div>
-        <button onClick={testFunction}>click</button>
+      <div className={styles.mainBanner}>
+        <Image
+          fill
+          src={imgSrc}
+          alt={'img'}
+        />
+      </div>
+      <div className={styles.midContainer}>
+        deploy test23232
       </div>
     </div>
-  )
+  );
 }
 
 export default TestContainer;
