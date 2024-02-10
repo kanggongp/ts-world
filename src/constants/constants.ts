3f5642faf19e2b262b39139f8dadaeb28a6772cc
kanggongp <gongkang24@gmail.com>
2024-02-11 00:32:17 +0900

    constants 파일 작성중

---
+++ src/constants/constants.ts
@@ -0,0 +1,192 @@
+export const CERTIFICATE = [
+  {
+    title: '영어',
+    name: 'TOEIC',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'TOEIC Speacking TEST',
+    score: ['선택', '1', '2', '3', '4', '5', '6', '7', '8'],
+  },
+  {
+    title: '영어',
+    name: 'OPIC',
+    score: ['선택', 'S', 'AH', 'AM', 'AL', 'IH', 'IM1', 'IM2', 'IM3', 'IL', 'NH', 'NM', 'NL'],
+  },
+  {
+    title: '영어',
+    name: 'IELTS',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'G-TELP(GBST)',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'G-TELP(GLT)',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'G-TELP(GST)',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'G-TELP(GST)',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'G-TELP(GWT)',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'TOEFL(CBT)',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'TOEFL(PBT)',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'TOEFL(iBT)',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'TEPS',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'TOEIC Speaking and Writing TEST',
+    score: ['선택', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  },
+  {
+    title: '영어',
+    name: 'Cambridge Certification FCE',
+    score: ['선택', 'grade A', 'grade B', 'grade C'],
+  },
+  {
+    title: '영어',
+    name: '무역영어',
+    score: ['선택', '1급', '2급', '3급'],
+  },
+  {
+    title: '영어',
+    name: 'TESOL',
+    score: ['선택', '취득'],
+  },
+  {
+    title: '영어',
+    name: 'NEW TEPS',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'SAT',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'ESPT',
+    score: ['선택', 'General', 'Teens', 'Junior1', 'Junior2', 'Kids'],
+  },
+  {
+    title: '영어',
+    name: 'OPI',
+    score: ['선택', 'S', 'AH', 'AM', 'AL', 'IH', 'IM1', 'IM2', 'IM3', 'IL', 'NH', 'NM', 'NL'],
+  },
+  {
+    title: '영어',
+    name: 'GRE',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'Cambridge Certification CAE',
+    score: ['선택', 'grade A', 'grade B', 'grade C'],
+  },
+  {
+    title: '영어',
+    name: 'TOEIC Bridge',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'ITT 비지니스 번역 자격증',
+    score: ['선택', '1급', '2급'],
+  },
+  {
+    title: '영어',
+    name: '영어 교원 자격증',
+    score: ['선택', '1급', '2급', '3급'],
+  },
+  {
+    title: '영어',
+    name: 'SPA(Speaking Proficiency Assessment)',
+    score: ['선택', 'Level 1', 'Level 2', 'Level 3', 'Level 4', 'Level 5', 'Level 6', 'Level 7', 'Level 8'],
+  },
+  {
+    title: '영어',
+    name: 'FLEX 영어',
+    score: ['선택', '3C', '3B', '3A', '2C', '2B', '2A', '1C', '1B', '1A'],
+  },
+  {
+    title: '영어',
+    name: 'GMAT',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'ITT 비지니스 통역 자격증',
+    score: ['선택', '1급', '2급'],
+  },
+  {
+    title: '영어',
+    name: 'SEPT',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'PTE Professional',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'AP',
+    score: ['선택', '1', '2', '3', '4', '5'],
+  },
+  {
+    title: '영어',
+    name: 'TCT 번역능력인증시험 영어',
+    score: ['선택', '1급', '2급', '3급'],
+  },
+  {
+    title: '영어',
+    name: 'PET(Preliminary English Test)',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'TEPS Speaking',
+    score: [],
+  },
+  {
+    title: '영어',
+    name: 'CPET',
+    score: ['선택', '취득'],
+  },
+  {
+    title: '영어',
+    name: 'TOEIC Bridge',
+    score: [],
+  },
+]
